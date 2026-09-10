d0f532bc62573078e803a0cb030fe915d92cdaac
DarkTailsPrower <CoinExPro@yandex.ru>
2026-09-10 20:51:28 +0300

    Добавлены файлы Qt 4.8.5 с поддержкой Git LFS

---
+++ Qt/src/corelib/global/qconfig.cpp
@@ -0,0 +1,33 @@
+/* Licensed */
+static const char qt_configure_licensee_str          [512 + 12] = "qt_lcnsuser=Open Source";
+static const char qt_configure_licensed_products_str [512 + 12] = "qt_lcnsprod=OpenSource";
+
+/* Build date */
+static const char qt_configure_installation          [11  + 12] = "qt_instdate=2026-09-10";
+
+static const char qt_configure_prefix_path_str       [512 + 12] = "qt_prfxpath=C:\\keymaster\\Roblox2016\\Library\\Qt";
+static const char qt_configure_documentation_path_str[512 + 12] = "qt_docspath=C:\\keymaster\\Roblox2016\\Library\\Qt\\doc";
+static const char qt_configure_headers_path_str      [512 + 12] = "qt_hdrspath=C:\\keymaster\\Roblox2016\\Library\\Qt\\include";
+static const char qt_configure_libraries_path_str    [512 + 12] = "qt_libspath=C:\\keymaster\\Roblox2016\\Library\\Qt\\lib";
+static const char qt_configure_binaries_path_str     [512 + 12] = "qt_binspath=C:\\keymaster\\Roblox2016\\Library\\Qt\\bin";
+static const char qt_configure_plugins_path_str      [512 + 12] = "qt_plugpath=C:\\keymaster\\Roblox2016\\Library\\Qt\\plugins";
+static const char qt_configure_imports_path_str      [512 + 12] = "qt_impspath=C:\\keymaster\\Roblox2016\\Library\\Qt\\imports";
+static const char qt_configure_data_path_str         [512 + 12] = "qt_datapath=C:\\keymaster\\Roblox2016\\Library\\Qt";
+static const char qt_configure_translations_path_str [512 + 12] = "qt_trnspath=C:\\keymaster\\Roblox2016\\Library\\Qt\\translations";
+static const char qt_configure_examples_path_str     [512 + 12] = "qt_xmplpath=C:\\keymaster\\Roblox2016\\Library\\Qt\\examples";
+static const char qt_configure_demos_path_str        [512 + 12] = "qt_demopath=C:\\keymaster\\Roblox2016\\Library\\Qt\\demos";
+/* strlen( "qt_lcnsxxxx") == 12 */
+#define QT_CONFIGURE_LICENSEE qt_configure_licensee_str + 12;
+#define QT_CONFIGURE_LICENSED_PRODUCTS qt_configure_licensed_products_str + 12;
+#define QT_CONFIGURE_PREFIX_PATH qt_configure_prefix_path_str + 12;
+#define QT_CONFIGURE_DOCUMENTATION_PATH qt_configure_documentation_path_str + 12;
+#define QT_CONFIGURE_HEADERS_PATH qt_configure_headers_path_str + 12;
+#define QT_CONFIGURE_LIBRARIES_PATH qt_configure_libraries_path_str + 12;
+#define QT_CONFIGURE_BINARIES_PATH qt_configure_binaries_path_str + 12;
+#define QT_CONFIGURE_PLUGINS_PATH qt_configure_plugins_path_str + 12;
+#define QT_CONFIGURE_IMPORTS_PATH qt_configure_imports_path_str + 12;
+#define QT_CONFIGURE_DATA_PATH qt_configure_data_path_str + 12;
+#define QT_CONFIGURE_TRANSLATIONS_PATH qt_configure_translations_path_str + 12;
+#define QT_CONFIGURE_EXAMPLES_PATH qt_configure_examples_path_str + 12;
+#define QT_CONFIGURE_DEMOS_PATH qt_configure_demos_path_str + 12;
+
